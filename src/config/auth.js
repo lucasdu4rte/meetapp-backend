f28db92ed3d91b857bdbf4ee0821930064d9abd6
Lucas Duarte <lucasdu4rte@gmail.com>
2019-08-05 23:16:36 -0300

    sequelize configure with authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f6b1b50317072730add747fad2fad231',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'lcf87fjebf2a',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
