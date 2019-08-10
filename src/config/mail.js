121c4b35917fcf419fa88b3a2b66b6aa7c068b9d
Lucas Duarte <lucasdu4rte@gmail.com>
2019-08-10 19:00:26 -0300

    nodemailer configure

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  post: 2525,
+  secure: false,
+  auth: {
+    user: 'ae42a70f7fa7f7',
+    pass: 'dfe872c42f418c',
+  },
+  default: {
+    from: 'Equipe MeetApp <noreply@meetapp.com>',
+  },
+};
